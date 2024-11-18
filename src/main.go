013b5fde5f584efed178e2003183e394cb202634
Felipe Abad <felipeabadmustillo@gmail.com>
2024-11-18 12:47:31 -0300

    EndPoint "/coins/:id"
    Se agrega el endpoint para monedas individuales con su info general. Ademas se utiliza bootstrap para estilizar la barra de busqueda.

--- src/main.go
+++ src/main.go
@@ -43,16 +43,37 @@ type CoinMarketDataResponse struct {
 	PriceBTC float64 `json:"current_price"`
 }
 
+// CoinDataResponse representa el formato de respuesta del endpoint "Coins" de CG.
+type CoinDataResponse struct {
+	ID          string   `json:"id"`
+	Symbol      string   `json:"symbol"`
+	Name        string   `json:"name"`
+	Categories  []string `json:"categories"`
+	Description struct {
+		English string `json:"en"`
+	} `json:"description"`
+	Image struct {
+		Thumbnail string `json:"thumb"`
+		Small     string `json:"small"`
+		Large     string `json:"large"`
+	} `json:"image"`
+	Origin      string `json:"country_origin"`
+	GenesisDate string `json:"genesis_date"`
+	WatchList   int    `json:"watchlist_portfolio_users"`
+	Rank        int    `json:"market_cap_rank"`
+}
+
 func main() {
 	router := gin.Default()
 
 	// Carga las plantillas desde la carpeta "templates" y los estilos desde la carpeta "static"
-	router.LoadHTMLFiles("templates/search.html", "templates/index.html")
+	router.LoadHTMLFiles("templates/search.html", "templates/index.html", "templates/moneda.html")
 	router.Static("/static", "./static")
 
 	//EndPoints
 	router.GET("/", getIndex)
-	router.GET("/search/:nameLike", getCoinsByNameLike)
+	router.GET("/coin/:id", getCoinByID)
+	router.GET("/coins/:nameLike", getCoinsByNameLike)
 
 	router.Run("localhost:8080")
 }
@@ -89,6 +110,30 @@ func getCoinsByNameLike(context *gin.Context) {
 	json.Unmarshal(body, &coinsMarketData)
 
 	context.HTML(http.StatusOK, "search.html", coinsMarketData) //Explicar bien como funcionan los templates
+}
+
+func getCoinByID(context *gin.Context) {
+	//Busca la moneda segun el id enviado por url y muestra todos sus datos en una pagina aparte.
+	var coinData CoinDataResponse
+	id := context.Param("id")
+
+	//Ahora hacer la request a CG /coins/:id con los ids filtrados para despues mostrar la lista
+	params := url.Values{}
+	params.Add("localization", "false")
+	params.Add("tickers", "false")
+	params.Add("community_data", "false")
+	params.Add("developer_data", "false")
+
+	req, _ := http.NewRequest("GET", fmt.Sprintf("%s?%s", ROOT+"/coins/"+id, params.Encode()), nil) //Averiguar bien que es lo de _ que maneja errores
+	req.Header.Add("accept", "application/json")                                                    //Averiguar bien porque es necesario este header
+	req.Header.Add(HEADER_AUTH_KEY, API_KEY)                                                        //Averiguar la forma mas segura de pasar la api key
+
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close() //Averiguar bien que es defer para documentar como caracteristica del lenguaje que creo que hayu algo ahi
+	body, _ := io.ReadAll(res.Body)
+	json.Unmarshal(body, &coinData)
+
+	context.HTML(http.StatusOK, "moneda.html", coinData) //Explicar bien como funcionan los templates
 }
 
 func getCoinIDs(nameFilter string) string {
